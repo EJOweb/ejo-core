867629892076173f98b4793b54de8286f571d019
EJOweb <erik@ejoweb.nl>
2015-08-21 17:17:41 +0200

    Added 'Intro' to visual editor styles-dropdown

--- includes/theme-tools/visual-editor-styles.php
+++ includes/theme-tools/visual-editor-styles.php
@@ -18,13 +18,25 @@ function ejo_mce_buttons_2( $buttons )
 //* Add custom styles to "Styles" dropdown
 function ejo_mce_before_init( $settings ) 
 {
-    $style_formats = array(
+    //* Get current styles or empty array
+    $style_formats = !empty($settings['style_formats']) ? json_decode( $settings['style_formats'] ) : array();
+
+    //* Make new styles
+    $new_style_formats = array(
         array(
             'title' => 'Button',
             'selector' => 'a',
             'classes' => 'button'
+        ),
+        array(
+            'title' => 'Intro',
+            'selector' => 'p',
+            'classes' => 'intro'
         )
     );
+
+    //* Combine new styles with current styles
+    $style_formats = array_merge($style_formats, $new_style_formats);
 
     $settings['style_formats'] = json_encode( $style_formats );
 
