3b9ef559adfd0b4369c38427cf70a5a70f3fea54
Erik Joling <erik@ejoweb.nl>
2016-08-31 22:35:28 +0200

    Improved custom colors

--- includes/custom-colors/custom-colors.php
+++ includes/custom-colors/custom-colors.php
@@ -76,6 +76,37 @@ final class EJO_Custom_Colors {
 	 */
 	public function body_class( $classes ) {
 
+		$primary_color = get_theme_mod( 'color_primary' );
+		$secondary_color = get_theme_mod( 'color_secondary' );
+
+		if ( $primary_color ) {
+
+			//* Get primary color contrast
+			$primary_color_contrast = $this->get_perceived_lightness( $primary_color );
+
+			$classes[] = 'primary-color-' . $primary_color_contrast;
+		}
+
+		if ( $secondary_color ) {
+
+			//* Get secondary color contrast
+			$secondary_color_contrast = $this->get_perceived_lightness( $secondary_color );
+
+			$classes[] = 'secondary-color-' . $secondary_color_contrast;
+		}
+
+
+		// write_log( $classes );
+
+		// write_log( '---------------------' );
+		// write_log( 'Contrast voor ' . $primary_color . ': ' . $primary_color_contrast );
+
+		// write_log( 'Contrast voor 000000: ' . $this->get_perceived_lightness( '000000' ) );
+		// write_log( 'Contrast voor ffffff: ' . $this->get_perceived_lightness( 'ffffff' ) );
+		// write_log( 'Contrast voor 777777: ' . $this->get_perceived_lightness( '777777' ) );
+		// write_log( 'Contrast voor f2912d: ' . $this->get_perceived_lightness( 'f2912d' ) );
+		// write_log( 'Contrast voor a9c857: ' . $this->get_perceived_lightness( 'a9c857' ) );
+
 		$classes[] = 'custom-colors';
 
 		return $classes;
@@ -124,17 +155,17 @@ final class EJO_Custom_Colors {
 	 */
 	public function get_primary_styles() {
 
-		$hex = get_theme_mod( 'color_primary', '' );
-		$rgb = join( ', ', hybrid_hex_to_rgb( $hex ) );
+		$hex1 = get_theme_mod( 'color_primary', '' );
+		$rgb1 = join( ', ', hybrid_hex_to_rgb( $hex1 ) );
+
+		$hex2 = get_theme_mod( 'color_secondary', '' );
+		$rgb2 = join( ', ', hybrid_hex_to_rgb( $hex2 ) );
 
 		// color
 		// $style .= "
 		// 		{ color: #{$hex}; } ";
 
-		$style = apply_filters( 'ejo_custom_color_style', '', $hex, $rgb );
-
-		write_log( 'filtered?' );
-		write_log( $style );
+		$style = apply_filters( 'ejo_custom_color_style', '', $hex1, $rgb1, $hex2, $rgb2 );
 
 		// // background-color
 		// $style .= ".page-header
@@ -235,6 +266,27 @@ final class EJO_Custom_Colors {
 	 */
 	public function cache_delete() {
 		wp_cache_delete( 'ejo_base_custom_colors' );
+	}
+
+	/** 
+	 * Get perceived (YIQ) dark or light
+	 * https://24ways.org/2010/calculating-color-contrast/
+	 * https://en.wikipedia.org/wiki/YIQ
+	 */
+	public function get_perceived_lightness( $hexcolor ) {
+
+		$r = hexdec(substr($hexcolor,0,2));
+		$g = hexdec(substr($hexcolor,2,2));
+		$b = hexdec(substr($hexcolor,4,2));
+
+		// $yiq = (($r*299)+($g*587)+($b*114))/1000;
+
+		$yiq = (($r * 299) +
+				($g * 587) +
+				($b * 114)) / 1000;
+
+		// return ($yiq >= 128) ? 'light' : 'dark';
+		return ($yiq >= 180) ? 'light' : 'dark';
 	}
 
 	/**
