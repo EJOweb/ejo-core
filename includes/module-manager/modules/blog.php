5b5b3fd8757f59e6046f71baf96fb0a0f61d8bbd
Erik Joling <erik@ejoweb.nl>
2017-01-25 14:06:27 +0100

    Fix bug with saving custom-taxonomy-term when blog-module is not active

--- includes/module-manager/modules/blog.php
+++ includes/module-manager/modules/blog.php
@@ -36,11 +36,19 @@ if ( EJO_Base_Module::is_inactive('blog') ) {
     //* Restrict access to posts screen (edit-posts, categories, tags, new-post)
     add_action( 'current_screen', function($current_screen) {
 
+        write_log($current_screen);
+
         //* Manipulations do not count for admin users
         if (current_user_can('manage_options'))
             return;
 
-        if ( 'post' == $current_screen->post_type ) {
+        /**
+         * Disallow access to post screens
+         *
+         * Fixes Bug: When saving taxonomy-term of a custom-post-type the $current_screen object will have `post` as post-type
+         * Solution: Only disallow when taxonomy is default (post_tag/category) or false
+         */
+        if ( $current_screen->post_type == 'post' && ($current_screen->taxonomy == 'post_tag' || $current_screen->taxonomy == 'category' || $current_screen->taxonomy == false) ) {
             wp_die( __( 'You are not allowed to access the posts section. Contact your developer if this doesn\'t seem right.' ) );
         }
     });
